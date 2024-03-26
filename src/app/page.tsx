5473da4ade686004d09669b84e156fe187bce00c
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-26 11:21:31 +0530

    added all the pngs and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Navbar from "@/components/global/navbar";
 export default function Home() {
   return (
     <main>
-      
+      <Navbar />
     </main>
   );
 }
