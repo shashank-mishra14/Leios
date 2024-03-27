c3d180a0700623bea7b3c04b3a090133fae9ba96
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-27 06:44:40 +0530

    sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -2,6 +2,7 @@
 import { usePathname } from 'next/navigation';
 import React from 'react'
 import Link from 'next/link';
+import clsx from 'clsx';
 import {
   Tooltip,
   TooltipContent,
@@ -9,6 +10,8 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 import { divMode } from '@tsparticles/engine';
+import { menuOptions } from '@/lib/constants';
+import { Ultra } from 'next/font/google';
 
 type Props = {}
 
@@ -23,10 +26,29 @@ const MenuOptions = (props: Props) => {
         Leios
       </Link>
       <TooltipProvider>
-        {menuOptions.map((option) => (
-          <div>
-
-          </div>}
+        {menuOptions.map((menuItem) => (
+          <ul key={menuItem.name}>
+            <Tooltip delayDuration={0}>
+            <TooltipTrigger>
+                  <li>
+                    <Link
+                      href={menuItem.href}
+                      className={clsx(
+                        'group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer',
+                        {
+                          'dark:bg-[#2F006B] bg-[#EEE0FF] ':
+                            pathName === menuItem.href,
+                        }
+                      )}
+                    >
+                      <menuItem.Component
+                        selected={pathName === menuItem.href}
+                      />
+                    </Link>
+                  </li>
+                </TooltipTrigger>
+            </Tooltip>
+          </ul>))}
       </TooltipProvider>
       </div>
     </nav>
