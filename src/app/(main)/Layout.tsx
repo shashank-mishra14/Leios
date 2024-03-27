a784c2b6ae5a330ae853dc3ca85e6fdad5c7fabf
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-27 07:06:03 +0530

    creating infobar

--- src/app/(main)/Layout.tsx
+++ src/app/(main)/Layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '@/components/sidebar'
 type Props = {children: React.ReactNode}
-
+import InfoBar from '@/components/infobar'
 const Layout = (props: Props) => {
   return (
     <div className='flex overflow-hidden h-screen'>
       <Sidebar />
         <div className='w-full'>
+          <InfoBar />
             {props.children}
         </div>
     </div>
---
+++ src/components/infobar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function InfoBar({}: Props) {
+  return (
+    <div>InfoBar</div>
+  )
+}
+
+export default InfoBar
