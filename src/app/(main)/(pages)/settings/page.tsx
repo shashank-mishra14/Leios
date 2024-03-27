441d9fa7cd045f0f27b65cbfc202ba38d85ae8a8
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-27 07:56:29 +0530

    creating setting page

---
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {}
+
+function Settings(props: Props) {
+  return (
+    <div className='flex flex-col gap-4'>
+        <h1 className='sticky top-0 z-[10] flex items-center
+        justify-between border-b- bg-background/50 p-6 text-4xl
+        backdrop-blur-lg'>
+            <span>Settings</span>
+        </h1>
+    </div>
+  )
+}
+
+export default Settings
