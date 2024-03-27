339946f07e88e9f7e89b8e6d846969bd29274b01
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-27 07:03:14 +0530

    added toggle for dark and light themes

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,35 +1,38 @@
 'use client'
-import { usePathname } from 'next/navigation';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
-import Link from 'next/link';
-import clsx from 'clsx';
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { divMode } from '@tsparticles/engine';
-import { menuOptions } from '@/lib/constants';
-import { Ultra } from 'next/font/google';
+} from '@/components/ui/tooltip'
+import { menuOptions } from '@/lib/constants'
+import clsx from 'clsx'
+import { Separator } from '@/components/ui/separator'
+import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
+import { ModeToggle } from '../global/mode-toggle'
 
 type Props = {}
 
 const MenuOptions = (props: Props) => {
-  const pathName= usePathname();
+  const pathName = usePathname()
+
   return (
-    <nav className='dark:bg-black h-screen overflow-scroll justify-between flex items-center
-    flex-col gap-10 py-6 px-2'>
-      <div className='flex items-center flex-col justify-content gap-8'>
-      <Link className='flex font-bold flex-row'
-      href='/'>
-        Leios
-      </Link>
-      <TooltipProvider>
-        {menuOptions.map((menuItem) => (
-          <ul key={menuItem.name}>
-            <Tooltip delayDuration={0}>
-            <TooltipTrigger>
+    <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
+      <div className="flex items-center justify-center flex-col gap-8">
+        <Link
+          className="flex font-bold flex-row "
+          href="/"
+        >
+          leios.
+        </Link>
+        <TooltipProvider>
+          {menuOptions.map((menuItem) => (
+            <ul key={menuItem.name}>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger>
                   <li>
                     <Link
                       href={menuItem.href}
@@ -47,9 +50,49 @@ const MenuOptions = (props: Props) => {
                     </Link>
                   </li>
                 </TooltipTrigger>
-            </Tooltip>
-          </ul>))}
-      </TooltipProvider>
+                <TooltipContent
+                  side="right"
+                  className="bg-black/10 backdrop-blur-xl"
+                >
+                  <p>{menuItem.name}</p>
+                </TooltipContent>
+              </Tooltip>
+            </ul>
+          ))}
+        </TooltipProvider>
+        <Separator />
+        <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <LucideMousePointerClick
+              className="dark:text-white"
+              size={18}
+            />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch
+              className="text-muted-foreground"
+              size={18}
+            />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <Database
+              className="text-muted-foreground"
+              size={18}
+            />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch
+              className="text-muted-foreground"
+              size={18}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center flex-col gap-8">
+        <ModeToggle />
       </div>
     </nav>
   )
