afeced1f58738a4348377304aa4e6329271ec528
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-05-20 22:09:24 +0530

    added the billing provider

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -10,6 +10,13 @@ function Settings(props: Props) {
         backdrop-blur-lg'>
             <span>Settings</span>
         </h1>
+        <div className='flex flex-col gap-10 p-6'>
+            <div>
+                <h2 className='text-2xl font-bold'>User Profile</h2>
+                <p className='text-base text-white/50'>
+                    Add or update your information </p>
+            </div>
+        </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { CheckIcon } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
-  //WIP: remove fault Image for home page
+  //WIP: remove fault IMAge for home page
   return (
     <main className="flex items-center justify-center flex-col">
       <Navbar />
@@ -47,7 +47,7 @@ export default function Home() {
       </section>
       <section className="mt-[-500px]">
         <LampComponent />
-        <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72 ">
+        <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
           <CardContainer className="inter-var ">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
               <CardItem
