d9996eb8684906f05f644817d15477a0ecb9f610
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-05-21 22:43:12 +0530

    changes

--- src/components/infobar/index.tsx
+++ src/components/infobar/index.tsx
@@ -13,24 +13,24 @@ import {
 } from '@/components/ui/tooltip'
 // import { UserButton } from '@clerk/nextjs'
 import { useBilling } from '@/providers/billing-provider'
-import { onPaymentDetails } from '@/app/(main)/(pages)/billing/_actions/payment-connecetions'
+// import { onPaymentDetails } from '@/app/(main)/(pages)/billing/_actions/payment-connections'
 
 type Props = {}
 
 const InfoBar = (props: Props) => {
   const { credits, tier, setCredits, setTier } = useBilling()
 
-  const onGetPayment = async () => {
-    const response = await onPaymentDetails()
-    if (response) {
-      setTier(response.tier!)
-      setCredits(response.credits!)
-    }
-  }
+  // const onGetPayment = async () => {
+  //   const response = await onPaymentDetails()
+  //   if (response) {
+  //     setTier(response.tier!)
+  //     setCredits(response.credits!)
+  //   }
+  // }
 
-  useEffect(() => {
-    onGetPayment()
-  }, [])
+  // useEffect(() => {
+  //   onGetPayment()
+  // }, [])
 
   return (
     <div className="flex flex-row justify-end gap-6 items-center px-4 py-4 w-full dark:bg-black ">
