c299aa91aad804be6e0d03eb31f7bb5fcc3d72fb
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-05-30 14:31:32 +0530

    added toast from shadcn and modal provider

--- src/app/(main)/Layout.tsx
+++ src/app/(main)/Layout.tsx
@@ -2,18 +2,17 @@ import React from 'react'
 import Sidebar from '@/components/sidebar'
 import InfoBar from '@/components/infobar'
 
-type Props = { children: React.ReactNode }
+type Props = {children: React.ReactNode}
 
 const Layout = (props: Props) => {
   return (
-    <div className="flex overflow-hidden h-screen">
+    <div className='flex overflow-hidden h-screen'>
       <Sidebar />
-      <div className="w-full">
-        <InfoBar />
-        {props.children}
-      </div>
+        <div className='w-full'>
+          <InfoBar />
+            {props.children}
+        </div>
     </div>
   )
 }
-
 export default Layout
